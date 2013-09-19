34e36ad8f08eb91c4ceb3b8654a547da095fbb0b
jdowning21 <jareddowning@hotmail.com>
2013-09-19 16:27:07 -0600

    Here is the Visual Studio Don Project

---
+++ Dons/Dons/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("Dons")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("UVU")> 
+<Assembly: AssemblyProduct("Dons")> 
+<Assembly: AssemblyCopyright("Copyright © UVU 2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9fa92458-a2cf-4fe2-a4e0-ac6cbde3ecd2")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
